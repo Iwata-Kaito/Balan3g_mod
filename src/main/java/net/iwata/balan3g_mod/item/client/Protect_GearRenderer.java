6b2b718c56737e53e6823f1529eb3fef2eb67a78
岩田 海斗 <ItohKaito@protonmail.com>
2026-02-06 19:34:26 +0900

    Armor作成途中-モデル反映できたがモデル自体に不備有 歩行時にLogとBootが逆向きに動く。

--- src/main/java/net/iwata/balan3g_mod/item/client/Protect_GearRenderer.java
+++ src/main/java/net/iwata/balan3g_mod/item/client/Protect_GearRenderer.java
@@ -1,10 +1,17 @@
 package net.iwata.balan3g_mod.item.client;
 
 import net.iwata.balan3g_mod.item.custom.Protect_GearItem;
+import net.minecraft.world.entity.EquipmentSlot;
 import software.bernie.geckolib.renderer.GeoArmorRenderer;
 
 public class Protect_GearRenderer extends GeoArmorRenderer<Protect_GearItem> {
     public Protect_GearRenderer() {
         super(new Protect_GearModel());
+    }
+
+    @Override
+    protected void applyBoneVisibilityBySlot(EquipmentSlot slot) {
+        super.applyBoneVisibilityBySlot(slot);
+        // superを呼ぶことで、ヘルメット時は armorHead 以外を隠すといったバニラ相当の制御が行われます。
     }
 }
